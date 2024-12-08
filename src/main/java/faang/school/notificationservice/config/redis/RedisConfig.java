6f7bee01e8b03a80d0e1afdeb5d72cbf21894fae
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-12-08 15:37:06 -0500

    BJS2-41801 - NewCommentNotification - Solved conflict, it still works

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,14 +1,13 @@
 package faang.school.notificationservice.config.redis;
 
+import faang.school.notificationservice.listener.CommentEventListener;
 import faang.school.notificationservice.listener.event.EventStartEventListener;
 import faang.school.notificationservice.listener.event.EventStartReminderEventListener;
-import faang.school.notificationservice.listener.CommentEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -23,20 +22,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     private final EventStartEventListener eventStartEventListener;
     private final EventStartReminderEventListener eventStartReminderEventListener;
-
     private final CommentEventListener commentEventListener;
 
     @Value("${spring.data.redis.host}")
     private String redisHost;
-
     @Value("${spring.data.redis.port}")
     private int redisPort;
+
     @Value("${spring.data.redis.channels.comment-channel}")
     private String topicComment;
-
     @Value("${spring.data.redis.channels.event-start-event-channel}")
     private String eventStartEventTopic;
-
     @Value("${spring.data.redis.channels.event-start-reminder-event-channel}")
     private String eventStartReminderEventTopic;
 
@@ -61,9 +57,8 @@ public class RedisConfig {
         container.setConnectionFactory(jedisConnectionFactory());
 
         addMessageListenerInContainer(eventStartEventListener, new ChannelTopic(eventStartEventTopic), container);
-        addMessageListenerInContainer(eventStartEventListener, eventStartEventTopic, container);
-        addMessageListenerInContainer(eventStartReminderEventListener, eventStartReminderEventTopic, container);
-
+        addMessageListenerInContainer(eventStartEventListener, new ChannelTopic(eventStartEventTopic), container);
+        addMessageListenerInContainer(eventStartReminderEventListener, new ChannelTopic(eventStartReminderEventTopic), container);
         addMessageListenerInContainer(commentEventListener, new ChannelTopic(topicComment), container);
         return container;
     }
